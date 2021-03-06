81c152512e04fd7a7d0c908612102361fca12db8
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-06 18:05:57 +0100

    Setup

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        TEXT: process.env.SECRET_TEXT,
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
