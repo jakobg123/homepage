15b3a650e08fc4a13431cd02ad3669a1e8c6b4df
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-07 18:27:39 +0100

    Refactor out to StartContainer from Index. add static props

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,18 @@
 import { PureComponent } from 'react';
+import menuData from '../serverData/menu.js';
+import knowledgeData from '../serverData/knowledge.js';
+// import menuData from '../serverData/menu.json';
+
+// import { fetchData } from '../utils/fetchData';
+// import { fetchData } from './fetch';
 
 // import LazyLoad from 'react-lazyload';
 // import { useInView } from 'react-intersection-observer';
 
-import Card from '../components/Card';
-
 import Header from '../components/Header';
-import Icon from '../components/Icon';
 import MobileNav from '../components/MobileNavButton';
 import styles from './index.module.scss';
 import { allIcons } from '../components/Icon/Icon';
-// import Footer from '../components/Footer';
 import classNames from 'classnames';
 
 // const Footer = React.lazy(() => import('../components/Footer'));
@@ -23,67 +25,41 @@ const Footer = dynamic(() => import('../components/Footer'), {
 
 import { Test } from '../components/Test';
 
+import StartContainer from '../containers/StartContainer';
+
 import React from 'react';
 
 class index extends PureComponent {
   constructor() {
     super();
+
+    // console.log(
+    //   'OUTPUT ÄR ~ file: index.jsx ~ line 35 ~ index ~ constructor ~ menu',
+    //   menu
+    // );
     state: {
       openNav: false;
+      menu: [];
     }
   }
 
+  // let {menu} = this.props;
+
   render() {
+    let { menu, knowledgeData } = this.props;
+
     return (
       <div>
         <Header />
-        {/* <MobileNav
-        // onClick={handleMobileNav}
-        // openNav={this.state.openNav}
-        // setOpenNav={setOpenNav}
-        /> */}
-        <section className={styles['TextWrapper']}>
-          <h1 className={styles['Title']}>
-            Frontend-utecklare som vill bygga din webb.
-          </h1>
-          <div className={styles['Grid']}>
-            <h2 className={styles['SubTitle']}>
-              Till sommar blir jag en färdigutbildad frontendutvecklare, och
-              letar nu jobb på en utvecklande webbyrå.
-            </h2>
-            <p className={styles['Paragraph']}>
-              <strong>Varmt välkommen till min hemsida!</strong>
-              Här showcase:ar jag lite av det jag har lärt mig i programmering
-              och webbutveckling efter 2 års studier.
-            </p>
-            <p className={styles['Paragraph']}>
-              Den här sidan är bl.a. gjord med Next.js och Typescript. Jag har
-              också jobbat med prestandaoptimering och tillgänglighet.
-            </p>
-          </div>
-          <button>Läs mer om hur sidan är byggd</button>
-          <div className={styles['Padding']}>
-            {Object.keys(allIcons).map((x, index) => (
-              <div className={styles['IconWrapper']} key={index}>
-                <Icon type={x} />
-              </div>
-            ))}
-          </div>
-          <Test text="okidoki" />
-          indexsidan
-          <ul>
-            {this.props.data.slice(0, 10).map((x) => (
-              <li key={x.id}>
-                <h3>{x.title}</h3>
-              </li>
-            ))}
-          </ul>
-          {/* <LazyLoad once offset={100}> */}
-          <Card />
-          {/* </LazyLoad> */}
-          {/* <Suspense fallback={<div>Laddar...</div>}>
-        </Suspense> */}
-        </section>
+        <div className={styles['StickyContainer']}>
+          <MobileNav
+            // onClick={handleMobileNav}
+            // openNav={this.state.openNav}
+            // setOpenNav={setOpenNav}
+            data={menu}
+          />
+        </div>
+        <StartContainer knowledge={knowledgeData} />
         <Footer />
       </div>
     );
@@ -131,12 +107,54 @@ class index extends PureComponent {
 // };
 
 export async function getStaticProps(content) {
-  let resp = await fetch('https://jsonplaceholder.typicode.com/todos/');
-  let data = await resp.json();
+  // console.log('OUTPUT ÄR ~ file: index.jsx ~ line 4 ~ menuData', menuData);
+
+  // let resp = await fetch('https://jsonplaceholder.typicode.com/todos/');
+  // let data = await resp.json();
+
+  // const menuData = await fetch('http://localhost:3000/api/menu/');
+
+  // let menuResp = await menuData.json();
+  // console.log(
+  //   'OUTPUT ÄR ~ file: index.jsx ~ line 161 ~ getStaticProps ~ menuResp',
+  //   menuResp
+  // );
+  // const menuResp = await fetchData('/menu/');
+  // console.log(
+  //   'OUTPUT ÄR ~ file: index.jsx ~ line 160 ~ getStaticProps ~ menuResp',
+  //   menuResp
+  // );
+
+  // const host = process.env.HOST;
+  // let data2 = await fetch(`http://${host}/api/menu/`);
+  // let resp2 = await data2.json();
+  // console.log('RESPPP', resp2);
+  // try {
+  //   // let menu = await fetchData(`/menu/`);
+  //   let menu = await fetch(`http://${host}/api/menu/`);
+  //   console.log(
+  //     'OUTPUT ÄR ~ file: index.jsx ~ line 173 ~ getStaticProps ~ menu',
+  //     menu
+  //   );
+  //   menu = await menu.json();
+  //   console.log(
+  //     'OUTPUT ÄR ~ file: index.jsx ~ line 175 ~ getStaticProps ~ menu',
+  //     menu
+  //   );
+  // } catch (err) {
+  //   console.error(err);
+  // }
+
+  // menu ? menu : (menu = []);
+  // menuData = JSON.stringify(menuData);
+  // const menu = menuData.json();
+  // console.log('menyn', menuData);
 
   return {
     props: {
-      data: data,
+      // data: data,
+      menu: menuData,
+      knowledgeData,
     },
   };
 }
