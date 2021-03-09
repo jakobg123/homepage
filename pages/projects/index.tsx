fe509954904ba630251a210b89d1494b9a8a835a
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-09 20:28:14 +0100

    Add ProjectsContainer to ProjectsPage

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 // import {useRouter} from "next/router";
+import ProjectsContainer from "../../containers/ProjectsContainer";
 
 
 const ProjectsPage = () => {
@@ -7,14 +8,16 @@ const ProjectsPage = () => {
     return (
         <div>
             <h1>Alla projekt</h1>
-        <Link href="/projects/[...id]" as={"/projects/first"}>
-            <a>First</a>
-        </Link>
-        <Link href="/projects/[...id]" as={"/projects/second"}>
-            <a>Second</a>
-        </Link>
+            <ProjectsContainer />
+            <div></div>
+            <Link href="/projects/[...id]" as={"/projects/first"}>
+                <a>First</a>
+            </Link>
+            <Link href="/projects/[...id]" as={"/projects/second"}>
+                <a>Second</a>
+            </Link>
 
-        <Link href="/">Tillbaka till start</Link>
+            <Link href="/">Tillbaka till start</Link>
         </div>
     )
 }
