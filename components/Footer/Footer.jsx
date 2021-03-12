3e7d5005b3b865bafae1cbc9cc0fa4a11f7faa16
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-12 22:49:38 +0100

    Add Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,26 +1,29 @@
 import React, { PureComponent } from 'react';
 import styles from './Footer.module.scss';
 
-class Footer extends PureComponent {
-  //   shouldComponentUpdate(nextProps, nextState) {
-  //     return false;
-  //   }
+// class Footer extends PureComponent {
+//   //   shouldComponentUpdate(nextProps, nextState) {
+//   //     return false;
+//   //   }
 
-  render() {
-    // console.log('FOOOTER');
-    return (
-      <div className={styles['Footer']}>
+//   render() {
+//     // console.log('FOOOTER');
+//     return (
+//       <div className={styles['Footer']}>
+//         <h2>Här är footern</h2>
+//       </div>
+//     );
+//   }
+// }
+const Footer = () => {
+  return (
+    <>
+      <div className={styles['Footer__TopWave']}></div>
+      <div className={styles['Footer__Content']}>
         <h2>Här är footern</h2>
       </div>
-    );
-  }
-}
-// const Footer = () => {
-//   return (
-//     <div className={styles['Footer']}>
-//       <h2>Här är footern</h2>
-//     </div>
-//   );
-// };
+    </>
+  );
+};
 
 export default Footer;
