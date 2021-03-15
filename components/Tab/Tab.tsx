c98ea02431ed32d851742d8cf0645ded43d34371
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-15 22:10:31 +0100

    Implement InfoContainer when Tab clicked

--- components/Tab/Tab.tsx
+++ components/Tab/Tab.tsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import DropDown from "../DropDown";
-import styles from "./Tab.module.scss";
-import Icon from "../Icon";
-import classNames from "classnames";
+import { useState } from 'react';
+import DropDown from '../DropDown';
+import styles from './Tab.module.scss';
+import Icon from '../Icon';
+import classNames from 'classnames';
 
-const Tab = ({ type, number, html, title, dark }) => {
+const Tab = ({ type, number, html, title, dark, id, onClick }) => {
+    console.log("OUTPUT ÄR ~ file: Tab.tsx ~ line 8 ~ Tab ~ id", id)
     const [isExpanded, setIsExpanded] = useState(false);
 
+    // console.log("OUTPUT ÄR ~ file: Tab.tsx ~ line 8 ~ Tab ~ type", type)
     const handleOnClick = () => {
+        // console.log("klickad");
         setIsExpanded(!isExpanded);
     };
 
     return (
-        <div className={classNames(styles["Tab"], { [styles["Tab--Dark"]]: dark })} role="button" onClick={handleOnClick}>
-            <div className={styles["Tab__IconWrapper"]}>
-                <Icon type={type} />
+        <div className={classNames(styles['Tab'],
+            { [styles['Tab--Dark']]: dark })} >
+            <button
+                className={classNames(styles['Tab__Button'],
+                    // { [styles['Tab--Dark']]: dark }
+                )}
+                // role="button"
+                // onClick={handleOnClick}
+                onClick={() => onClick(id)}
+            >
+                <span className="sr-only">Läs om mina erfarenheter med {type}</span>
+            </button>
+            <div className={styles['Tab__IconWrapper']}>
+                <Icon type={type} color={"Primary"} modifier={[styles["Tab__Height"]]} />
             </div>
-
-            <div className={styles["Tab__TextAndDropDownWrapper"]}>
-                <div className={styles["Tab__TextWrapper"]}>
-                    <h3 className={styles["Tab__Title"]}>{title}</h3>
-                    <strong className={styles["Tab__Knowledge"]}>Nivå: {number}</strong>
+            <div className={styles['Tab__TextAndDropDownWrapper']}>
+                <div className={styles['Tab__TextWrapper']}>
+                    <h3 className={styles['Tab__Title']}>{title}</h3>
+                    <p className={styles['Tab__Knowledge']}>
+                        <strong className={classNames(styles['Tab__Knowledge--Strong'], { [styles["Tab__Knowledge--Strong--Dark"]]: dark })}>{number}</strong> /10</p>
                 </div>
-                {!!html &&
-                    <DropDown html={html} dark={dark} onClick={handleOnClick} isExpanded={isExpanded} />
-                }
+                {!!html && (
+                    <span className={styles['Tab__DropDownWrapper']}>
+                        <DropDown
+                            html={html}
+                            dark={dark}
+                            onClick={handleOnClick}
+                            isExpanded={isExpanded}
+                        />
+                    </span>
+                )}
             </div>
-        </div>
-    )
-}
 
-export default Tab
+        </div>
+    );
+};
+
+export default Tab;
--- components/TabList/TabList.tsx
+++ components/TabList/TabList.tsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
+
 import Tab from "../Tab";
 import styles from "./TabList.module.scss";
 import classNames from "classnames";
 
+const InfoContainer = ({ info }) => (
+
+    info.map(({ title, html }) => (
+        <>
+            <h3 className={styles["TabList__InfoTitle"]}>{title}</h3>
+            <div className={styles["TabList__InfoText"]} dangerouslySetInnerHTML={{ __html: html }}></div>
+        </>
+    ))
+)
+
 const TabList = ({ knowledge, title, dark = false }) => {
+
+    const [currentKnowledge, setCurrentKnowledge] = useState([]);
+
+    const handleClick = (id) => {
+        const info = knowledge.filter(x => x.id === id);
+        setCurrentKnowledge(info)
+    };
+
+
     return (
-        <div className={classNames(styles["TabList"], { [styles["TabList--Dark"]]: dark })}>
-            <h3 className={styles["TabList__Title"]}>{title}</h3>
-            <ul className={styles["TabList__List"]}>
-                {!!knowledge.length && knowledge.map((item, index) => (
-                    <li key={index} className={styles["TabList__ListItem"]}>
-                        <Tab {...item} dark={dark} />
-                    </li>
-                ))}
-            </ul>
+        <div className={styles["TabList"]}>
+            <div className={classNames(styles["TabList__CardsContainer"], { [styles["TabList__CardsContainer--Dark"]]: dark })}>
+                <h3 className={styles["TabList__Title"]}>{title}</h3>
+                <ul className={styles["TabList__List"]}>
+                    {!!knowledge.length && knowledge.map((item, index) => (
+                        <li key={index} className={styles["TabList__ListItem"]}>
+                            <Tab {...item} dark={dark} onClick={handleClick} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className={classNames(styles["TabList__InfoContainer"], { [styles["TabList__Text--Dark"]]: dark })}>
+                {!!currentKnowledge.length && <InfoContainer info={currentKnowledge} />}
+            </div>
         </div>
     )
 }
