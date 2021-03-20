47510504b27e4f076a306e4ecdcacb9eb6dc6ea1
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-20 16:21:53 +0100

    Add meta content due to SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,7 +39,10 @@ class index extends PureComponent {
             content="Snart är jag en nyexad junior Frontendutvecklare. Nu söker jag jobb där jag får arbeta med webbutveckling och programmering. Här är min portfolio-sajt."
             // content="I juni 2021 tar jag examen i Frontendutveckling på IT-Högskolan. Jag söker nu ett jobb där jag får arbeta med webbutveckling och växa inom programmering."
           />
-          <meta name="viewport" content="user-scalable=yes" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, user-scalable=yes"
+          />
         </Head>
         <div className={styles['Base']}>
           <header className={styles['Base__Header']}>
