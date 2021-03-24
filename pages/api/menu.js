e2362da35d276fe3a4b44085670d8800d32e880b
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-25 00:04:37 +0100

    Remove redundant api

--- pages/api/menu.js
+++
@@ -1,8 +0,0 @@
-export default (req, res) => {
-    res.status(200).json([
-        { link: 'om sidan', url: '/projects/' },
-        { link: 'cv', url: '/projects/' },
-        { link: 'om mig', url: '/projects/' },
-        { link: 'kontakt', url: '/projects/' },
-    ]);
-};
