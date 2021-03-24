bda15d0924a49a31ad4de8366690b2c54a492b82
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-24 23:15:29 +0100

    Add large arrow to TabList in Knowledge, style fixes

--- components/TabList/TabList.tsx
+++ components/TabList/TabList.tsx
@@ -3,8 +3,10 @@ import { useState, useEffect, useRef } from "react";
 import Tab from "../Tab";
 import styles from "./TabList.module.scss";
 import classNames from "classnames";
+import Icon from "../Icon";
 
 const InfoContainer = ({ info }) => {
+
     const [show, setShow] = useState(false)
     useEffect(() => {
         if (show) {
@@ -20,7 +22,7 @@ const InfoContainer = ({ info }) => {
     return (
         info.map(({ title, html }) => (
             <>
-                <h3 className={styles["TabList__InfoTitle"]}>{title}</h3>
+                <h5 className={styles["TabList__InfoTitle"]}>{title}</h5>
                 <div className={classNames(styles["TabList__InfoText"], { [styles["TabList__InfoText--Show"]]: show })} dangerouslySetInnerHTML={{ __html: html }}></div>
             </>
         ))
@@ -46,7 +48,7 @@ const TabList = ({ knowledge, title, dark = false }) => {
     return (
         <div className={classNames(styles["TabList"], { [styles["TabList--Dark"]]: dark })}>
             <div className={classNames(styles["TabList__CardsContainer"], { [styles["TabList__CardsContainer--Dark"]]: dark })}>
-                <h3 className={styles["TabList__Title"]}>{title}</h3>
+                <h4 className={styles["TabList__Title"]}>{title}</h4>
                 <ul className={styles["TabList__List"]}>
                     {!!knowledge.length && knowledge.map((item, index) => (
                         <li key={index} className={styles["TabList__ListItem"]}>
@@ -55,13 +57,25 @@ const TabList = ({ knowledge, title, dark = false }) => {
                     ))}
                 </ul>
             </div>
-            <div className={classNames(styles["TabList__InfoContainer"], { [styles["TabList__Text--Dark"]]: dark })}>
-                {!!currentKnowledge.length && (
+            <div className={classNames(styles["TabList__InfoContainer"], { [styles["TabList__InfoContainer--Dark"]]: dark })}>
+                {!!currentKnowledge.length ? (
                     <InfoContainer info={currentKnowledge} key={currentKnowledge[0].id} />
                 )
+                    :
+                    <div className={classNames(styles["TabList__InfoStartWrapper"], { [styles["TabList__InfoStartWrapper--Dark"]]: dark })} >
+                        <p aria-hidden="true" className={styles["TabList__InfoStartText"]} > Klicka på den teknologi du vill veta mer om.</p>
+                        <div aria-hidden="true" className={styles["TabList__InfoStartIconWrapper"]}>
+                            {dark ? <Icon type={"arrowNext"} color={"Primary"} /> : <Icon type={"arrowPrev"} color={"Black"} />}
+
+                        </div>
+                    </div>
                 }
+                {/* {!!currentKnowledge.length && (
+                    <InfoContainer info={currentKnowledge} key={currentKnowledge[0].id} />
+                )
+                } */}
             </div>
-        </div>
+        </div >
     )
 }
 
