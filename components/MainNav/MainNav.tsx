a00c327022eb3ae7c6f503951721557291a0e1bd
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-25 00:43:29 +0100

    Clean up MainNav

--- components/MainNav/MainNav.tsx
+++ components/MainNav/MainNav.tsx
@@ -1,29 +1,29 @@
 import styles from "./MainNav.module.scss";
-
 import Link from "next/link";
 
 const MainNav = () => {
+    const links = [
+        { target: "#technology", label: "Teknologier" },
+        { target: "#internship", label: "Praktik" },
+        { target: "#contact", label: "Kontakt" }
+    ];
+
     return (
         <nav className={styles['MainNav']}>
             <figure className={styles['MainNav__Logo']} >
                 <img src="/logoPrimary.svg" alt="Jakob Gauffin, sidans logotyp" width="200px" height="40px" />
             </figure>
             <ul className={styles['MainNav__List']}>
-                <li className={styles['MainNav__ListItem']}>
-                    <Link href="#technology">
-                        <a className={styles['MainNav__Link']} >Teknologier</a>
-                    </Link>
-                </li>
-                <li className={styles['MainNav__ListItem']}>
-                    <Link href="#internship" >
-                        <a className={styles['MainNav__Link']} >Praktik</a>
-                    </Link>
-                </li>
-                <li className={styles['MainNav__ListItem']}>
-                    <Link href="#contact" >
-                        <a className={styles['MainNav__Link']} >Kontakt</a>
-                    </Link>
-                </li>
+                {!!links.length && (
+                    links.map(({ target, label }) => (
+                        <li className={styles['MainNav__ListItem']}>
+                            <Link href={target}>
+                                <a className={styles['MainNav__Link']} >{label}</a>
+                            </Link>
+                        </li>
+
+                    ))
+                )}
             </ul>
         </nav>
     )
