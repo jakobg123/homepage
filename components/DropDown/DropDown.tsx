799e0775f5761ee5dedcb1612d7fc7223acfce1a
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-25 00:17:19 +0100

    Clean upp DropDown

--- components/DropDown/DropDown.tsx
+++ components/DropDown/DropDown.tsx
@@ -1,32 +1,18 @@
-import { useState } from 'react';
 import Icon from '../Icon';
 import styles from './DropDown.module.scss';
 import classNames from 'classnames';
 
-const DropDown = ({ html, dark, onClick, isExpanded }) => {
-    // const [isExpanded, setIsExpanded] = useState(false);
-
-    // const handleOnClick = () => {
-    //     setIsExpanded(!isExpanded);
-    // };
-
+const DropDown = ({ html, dark, isExpanded }) => {
     return (
         <div
             className={classNames(
                 styles['DropDown'],
                 {
-                    [styles['DropDown--Expanded']]: isExpanded,
-                },
-                {
                     [styles['DropDown--Dark']]: dark,
                 }
             )}>
             <span
-                className={classNames(styles['DropDown__Button'], {
-                    [styles['DropDown__Button--Expanded']]: isExpanded,
-                })}
-            // onClick={onClick}
-            >
+                className={styles['DropDown__Button']}>
                 <strong className={styles['DropDown__ButtonText']}>Läs mer</strong>
                 <span
                     className={classNames(styles['DropDown__IconWrapper'], {
