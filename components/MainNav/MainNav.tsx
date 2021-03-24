4e073ac5ec779aa526d3ef3c5799f68a9a4dbdfc
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-24 23:13:50 +0100

    Add site logo to MainNav

--- components/MainNav/MainNav.tsx
+++ components/MainNav/MainNav.tsx
@@ -5,6 +5,9 @@ import Link from "next/link";
 const MainNav = () => {
     return (
         <nav className={styles['MainNav']}>
+            <figure className={styles['MainNav__Logo']} >
+                <img src="/logoPrimary.svg" alt="Jakob Gauffin, sidans logotyp" width="200px" height="40px" />
+            </figure>
             <ul className={styles['MainNav__List']}>
                 <li className={styles['MainNav__ListItem']}>
                     <Link href="#technology">
@@ -14,6 +17,11 @@ const MainNav = () => {
                 <li className={styles['MainNav__ListItem']}>
                     <Link href="#internship" >
                         <a className={styles['MainNav__Link']} >Praktik</a>
+                    </Link>
+                </li>
+                <li className={styles['MainNav__ListItem']}>
+                    <Link href="#contact" >
+                        <a className={styles['MainNav__Link']} >Kontakt</a>
                     </Link>
                 </li>
             </ul>
