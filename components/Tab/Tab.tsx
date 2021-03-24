34e2a071255101f7a5d2dd8c860fbaaa31d6ef37
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-25 00:58:11 +0100

    Clean up Tab

--- components/Tab/Tab.tsx
+++ components/Tab/Tab.tsx
@@ -5,43 +5,45 @@ import Icon from '../Icon';
 import classNames from 'classnames';
 
 const Tab = ({ type, number, html, title, dark, id, onClick }) => {
-    // console.log("OUTPUT ÄR ~ file: Tab.tsx ~ line 8 ~ Tab ~ id", id)
     const [isExpanded, setIsExpanded] = useState(false);
 
-    // console.log("OUTPUT ÄR ~ file: Tab.tsx ~ line 8 ~ Tab ~ type", type)
     const handleOnClick = (id) => {
         // for dropdown in mobileview
         setIsExpanded(!isExpanded);
 
         // for displaying text in infoview in desktopview
-        onClick(id)
+        onClick(id);
     };
 
     return (
-        <div className={classNames(styles['Tab'],
-            { [styles['Tab--Dark']]: dark })}
-        >
+        <div className={classNames(styles['Tab'], { [styles['Tab--Dark']]: dark })}>
             <button
-                className={classNames(styles['Tab__Button'],
-                    // { [styles['Tab--Dark']]: dark }
-                )}
-                // role="button"
-                // onClick={handleOnClick}
-                onClick={() => handleOnClick(id)}
-            // onClick={() => onClick(id)}
-            >
+                className={styles['Tab__Button']}
+                onClick={() => handleOnClick(id)}>
                 <span className="sr-only">Läs om mina erfarenheter med {type}</span>
             </button>
-            <span className={classNames(styles['Tab__Overlay'],
-                { [styles['Tab__Overlay--Dark']]: dark })}></span>
+            <span
+                className={styles['Tab__Overlay']}></span>
             <div className={styles['Tab__IconWrapper']}>
-                <Icon type={type} color={dark ? "White" : "Black"} modifier={[styles["Tab__Height"]]} hoverEffect />
+                <Icon
+                    type={type}
+                    color={dark ? 'White' : 'Black'}
+                    modifier={[styles['Tab__Height']]}
+                    hoverEffect
+                />
             </div>
             <div className={styles['Tab__TextAndDropDownWrapper']}>
                 <div className={styles['Tab__TextWrapper']}>
                     <h3 className={styles['Tab__Title']}>{title}</h3>
                     <p className={styles['Tab__Knowledge']}>
-                        <strong className={classNames(styles['Tab__Knowledge--Strong'], { [styles["Tab__Knowledge--Strong--Dark"]]: dark })}>{number}</strong>/10</p>
+                        <strong
+                            className={classNames(styles['Tab__Knowledge--Strong'], {
+                                [styles['Tab__Knowledge--Strong--Dark']]: dark,
+                            })}>
+                            {number}
+                        </strong>
+                    /10
+                    </p>
                 </div>
                 {!!html && (
                     <span className={styles['Tab__DropDownWrapper']}>
@@ -54,7 +56,6 @@ const Tab = ({ type, number, html, title, dark, id, onClick }) => {
                     </span>
                 )}
             </div>
-
         </div>
     );
 };
