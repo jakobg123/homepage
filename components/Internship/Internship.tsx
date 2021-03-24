70d4d6d3dce09bf612f8b1717739749e7e21a166
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-25 00:33:16 +0100

    Clean up Internship

--- components/Internship/Internship.tsx
+++ components/Internship/Internship.tsx
@@ -1,27 +1,9 @@
-import React from 'react';
-
-import { useInView } from 'react-intersection-observer';
-import styles from './Internship.module.scss';
 import classNames from 'classnames';
-import { IImageProps, IMediaQueries } from '../../types/types';
-import Image from "../Image";
+
+import styles from './Internship.module.scss';
 import Icon from "../Icon";
 
 const Internship = ({ data, inView }) => {
-  const image: IImageProps = {
-    // src: '/images/goldenGateSmall.jpg',
-    src: '/images/temp/frojd_orginal.jpg',
-    // src: '/images/tuscany1.jpg',
-    alt: 'Personer som arbetar på Fröjd',
-    width: 1000,
-    height: 555,
-    // src: '/images/glassOptXl.webp',
-    // // src: '/images/tuscany1.jpg',
-    // alt: 'Cool ljus glaskula',
-    // width: 640,
-    // height: 960,
-    // loading: 'lazy',
-  };
 
   return (
     <div
@@ -42,11 +24,6 @@ const Internship = ({ data, inView }) => {
           <div
             className={styles['Internship__Paragraph']}
             dangerouslySetInnerHTML={{ __html: data.html2 }}></div>
-          {/* <div className={styles["Internship__Outer"]}>
-            <div className={styles["Internship__ImageWrapper"]}>
-              <Image {...image} />
-            </div>
-          </div> */}
         </div>
         <div className={styles["Internship__ImageAndTextWrapper"] + " " + styles["Internship__ImageAndTextWrapper--Desktop"]}>
           <div
@@ -57,11 +34,6 @@ const Internship = ({ data, inView }) => {
               <Icon type={"frojd"} color={"White"} modifier={[styles["Internship__Width"]]} />
             </div>
           </div>
-          {/* <div className={styles["Internship__Outer"]}>
-            <div className={styles["Internship__ImageWrapper"]}>
-              <Image {...image} />
-            </div>
-          </div> */}
         </div>
         <div
           className={styles['Internship__Paragraph']}
