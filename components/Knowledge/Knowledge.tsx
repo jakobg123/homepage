b166fa330df81a1ab04754ae49ac2ed7941f5d72
jakobg123 <jakob.gauffin45@gmail.com>
2021-03-28 00:05:21 +0100

    Make backend knowledge fade in

--- components/Knowledge/Knowledge.tsx
+++ components/Knowledge/Knowledge.tsx
@@ -1,19 +1,30 @@
 import TabList from "../TabList";
 
 import styles from "./Knowledge.module.scss";
+import classNames from "classnames";
 
+import { useInView } from 'react-intersection-observer';
 
 const Knowledge = ({ knowledge }) => {
+    const { ref, inView, entry } = useInView({
+        threshold: 0.05,
+        triggerOnce: true,
+    });
+
     return (
         <div className={styles['Knowledge']}>
             <TabList knowledge={knowledge.frontend} title={'Frontend'} />
+
+
             <div
-                className={styles['Knowledge__TabListWrapper--Backend']}>
-                <TabList
-                    knowledge={knowledge.backend}
-                    title={'Backend'}
-                    dark={true}
-                />
+                className={classNames(styles['Knowledge__TabListWrapper--Backend'])}>
+                <div ref={ref} className={classNames(styles['Knowledge__TabListWrapper--Hide'], { [styles['Knowledge__TabListWrapper--Show']]: inView })}>
+                    <TabList
+                        knowledge={knowledge.backend}
+                        title={'Backend'}
+                        dark={true}
+                    />
+                </div>
             </div>
         </div>
     )
