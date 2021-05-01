a2e9d9cf97e4f68848080d808da28c7be60b43e8
jakobg123 <jakob.gauffin45@gmail.com>
2021-05-01 13:07:58 +0200

    Autoformat code in Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,7 +5,7 @@ import styles from './Header.module.scss';
 import classNames from 'classnames';
 
 import { IImageProps } from '../../types/types';
-import {IMainNavProps} from "../MainNav/MainNav";
+import { IMainNavProps } from "../MainNav/MainNav";
 
 export interface IHeaderProps {
     heroImage: IImageProps,
@@ -17,7 +17,7 @@ const Header: React.FC<IHeaderProps> = ({ heroImage, mainNavData }) => {
     return (
         <div className={styles['Header']}>
             <div className={styles['Header__MainNavWrapper']}>
-                <MainNav {...mainNavData}/>
+                <MainNav {...mainNavData} />
             </div>
             <div
                 className={classNames(styles['Header__ImageWrapper'])}>
