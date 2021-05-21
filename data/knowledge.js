47f02285b22e3a7dbefb19a82882775400087409
jakobg123 <jakob.gauffin45@gmail.com>
2021-05-22 00:39:38 +0200

    Change SCSS knowledgetext to test deployment

--- data/knowledge.js
+++ data/knowledge.js
@@ -4,80 +4,70 @@ export default {
             type: 'html',
             title: 'HTML',
             number: '8',
-            html:
-                '<p>Jag har mycket god kunskap i HTML och lägger stor vikt vid att koden ska vara semantiskt korrekt och tillgänglighetsanpassad. Jag jobbar med axeDevTools och Lighthouse och har även gått igenom Funka-rapporter på min praktik för att få kunskap om vad som utgör bra HTML.</p>',
+            html: '<p>Jag har mycket god kunskap i HTML och lägger stor vikt vid att koden ska vara semantiskt korrekt och tillgänglighetsanpassad. Jag jobbar med axeDevTools och Lighthouse och har även gått igenom Funka-rapporter på min praktik för att få kunskap om vad som utgör bra HTML.</p>',
             id: 1,
         },
         {
             type: 'css',
             title: 'CSS',
             number: '8',
-            html:
-                '<p>Har mycket god kunskap i CSS, och framför allt med SASS som preprocessor.<p>',
+            html: '<p>Har mycket god kunskap i CSS, och framför allt med SASS som preprocessor.<p>',
             id: 2,
         },
         {
             type: 'sass',
             title: 'SASS',
             number: '8',
-            html:
-                '<p>Jag har jobbat väldigt mycket med SCSS enligt BEM-praxis och känner mig äntligen säker på styling, gällande selektorer, extensions och mixins.</p><p>Jag har även erfarenhet av Styled Components och @emotion och strävar hela tiden mot att få till en snygg, responsiv styling med många återanvändbara SASS-variabler.</p>',
+            html: '<p>Jag har jobbat väldigt mycket med SCSS enligt BEM-praxis och känner mig säker på styling, gällande selektorer, extensions och mixins.</p><p>Jag har även erfarenhet av Styled Components och @emotion och strävar hela tiden mot att få till en snygg, responsiv styling med många återanvändbara SASS-variabler.</p>',
             id: 3,
         },
         {
             type: 'javascript',
             title: 'Javascript',
             number: '8',
-            html:
-                '<p>Hela utbildningen har präglats av mycket Javascript. Jag känner att jag behärskar vanilla js och är kunnig inom JQuery.</p><p>Jag kan grundläggande principer såsom hur koden exekveras och har bekantat mig med funktionell programmering.</p>',
+            html: '<p>Hela utbildningen har präglats av mycket Javascript. Jag känner att jag behärskar vanilla js och är kunnig inom JQuery.</p><p>Jag kan grundläggande principer såsom hur koden exekveras och har bekantat mig med funktionell programmering.</p>',
             id: 4,
         },
         {
             type: 'react',
             title: 'React',
             number: '8',
-            html:
-                '<p>Jag har arbetat mycket med React och känner att jag behärskar grunderna väl.</p><p>Jag har två större React-projekt att visa upp: framför allt den här sajten där koden finns <a href="https://github.com/jakobg123/homepage">här på GitHub-repot</a>.</p><p>Sedan gjorde jag <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster-wars</a> våren 2020, alltså nästan ett år sedan.</p><p>Koden till Hamster Wars är inte den bästa, men finns på mitt <a href="https://github.com/iths-jakob-gauffin/hamsterwars_slutprojekt">skol-git-repo</a>. <p>Hamster-wars var ett fullstackprojekt där jag byggde ett REST-api med CRUD-funktionalitet i Node.js med Firebase som databas och React som frontendlager.</p><p>Uppgiften bestod i att göra en sajt där användaren rankar söta hamstrar, där resultat registreras och presenteras på olika sätt utifrån hur man konsumerar api:et. Jag arbetade även med Redux, React-Spring och @emotion vilket var trevligt.</p><p>Tyvärr gjorde jag inte sidan responsiv utan den är enbart anpassad för mobil.</p>',
+            html: '<p>Jag har arbetat mycket med React och känner att jag behärskar grunderna väl.</p><p>Jag har två större React-projekt att visa upp: framför allt den här sajten där koden finns <a href="https://github.com/jakobg123/homepage">här på GitHub-repot</a>.</p><p>Sedan gjorde jag <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster-wars</a> våren 2020, alltså nästan ett år sedan.</p><p>Koden till Hamster Wars är inte den bästa, men finns på mitt <a href="https://github.com/iths-jakob-gauffin/hamsterwars_slutprojekt">skol-git-repo</a>. <p>Hamster-wars var ett fullstackprojekt där jag byggde ett REST-api med CRUD-funktionalitet i Node.js med Firebase som databas och React som frontendlager.</p><p>Uppgiften bestod i att göra en sajt där användaren rankar söta hamstrar, där resultat registreras och presenteras på olika sätt utifrån hur man konsumerar api:et. Jag arbetade även med Redux, React-Spring och @emotion vilket var trevligt.</p><p>Tyvärr gjorde jag inte sidan responsiv utan den är enbart anpassad för mobil.</p>',
             id: 5,
         },
         {
             type: 'nextjs',
             title: 'Next.js',
             number: '7',
-            html:
-                '<p>Jag har jobbat mycket i Next under min praktik på Fröjd och den här sajten är skriven med ramverket.</p><p>Jag ser flera fördelar med Next. Man kan få statiskt genererade sidor vilket ger blixtsnabba sidor, samtidigt kan man nyttja server-side-rendering, som i många fall är att föredra vid API-anrop. Dessutom är det enkelt att bygga ett REST-api med Next, så <span lang="en">"what\'s not to like"?</span></p>',
+            html: '<p>Jag har jobbat mycket i Next under min praktik på Fröjd och den här sajten är skriven med ramverket.</p><p>Jag ser flera fördelar med Next. Man kan få statiskt genererade sidor vilket ger blixtsnabba sidor, samtidigt kan man nyttja server-side-rendering, som i många fall är att föredra vid API-anrop. Dessutom är det enkelt att bygga ett REST-api med Next, så <span lang="en">"what\'s not to like"?</span></p>',
             id: 6,
         },
         {
             type: 'typescript',
             title: 'Typescript',
             number: '6',
-            html:
-                '<p>Jag håller på att lära mig Typescript just nu.</p><p>Den här sidan är det första större projektet som jag skriver i Typescript. Av tidsskäl är den inte helt konverterad ännu, men sidan uppfyller hittills "noImplicitAny"-kravet.</p><p> Jag uppskattar tydligheten med TypeScript och den direkta feedbacken.</p>',
+            html: '<p>Jag håller på att lära mig Typescript just nu.</p><p>Den här sidan är det första större projektet som jag skriver i Typescript. Av tidsskäl är den inte helt konverterad ännu, men sidan uppfyller hittills "noImplicitAny"-kravet.</p><p> Jag uppskattar tydligheten med TypeScript och den direkta feedbacken.</p>',
             id: 7,
         },
         {
             type: 'reactSpring',
             title: 'React-Spring',
             number: '5',
-            html:
-                '<p>React-Spring är ett animationsbibliotek som gör det möjligt att få till riktigt snygga visuella effekter på webben.</p><p>Jag har jobbat en del med biblioteket och har för avsikt att bli ännu bättre på det eftersom jag tycker det är väldigt kul med animationer.</p><p>Än så länge finns ingen React-Spring på den här sidan, men jag arbetade med det bl.a. på mitt skolprojekt <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>, som jag gjorde våren 2020.</p>',
+            html: '<p>React-Spring är ett animationsbibliotek som gör det möjligt att få till riktigt snygga visuella effekter på webben.</p><p>Jag har jobbat en del med biblioteket och har för avsikt att bli ännu bättre på det eftersom jag tycker det är väldigt kul med animationer.</p><p>Än så länge finns ingen React-Spring på den här sidan, men jag arbetade med det bl.a. på mitt skolprojekt <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>, som jag gjorde våren 2020.</p>',
             id: 8,
         },
         {
             type: 'redux',
             title: 'Redux',
             number: '5',
-            html:
-                '<p>Jag har bekantat mig med Redux på ett flertal egna mindre projekt för att lära mig hur state-management-systemet fungerar.</p><p>Bl.a. vävde jag in Redux i skolprojektet <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>, men hittills har jag inte byggt någon större applikation med det.</p>',
+            html: '<p>Jag har bekantat mig med Redux på ett flertal egna mindre projekt för att lära mig hur state-management-systemet fungerar.</p><p>Bl.a. vävde jag in Redux i skolprojektet <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>, men hittills har jag inte byggt någon större applikation med det.</p>',
             id: 9,
         },
         {
             type: 'github',
             title: 'Git & GitHub',
             number: '7',
-            html:
-                '<p>Jag har arbetat mycket med Git under dessa två år och det senaste halvåret har jag arbetat med det dagligen.</p><p>Jag vill säga att jag behärskar grunderna väl.</p><p>På Fröjd har jag också fått kunskap om Git Flow, som handlar om hur man bör arbeta med Git på ett professionellt sätt.</p>',
+            html: '<p>Jag har arbetat mycket med Git under dessa två år och det senaste halvåret har jag arbetat med det dagligen.</p><p>Jag vill säga att jag behärskar grunderna väl.</p><p>På Fröjd har jag också fått kunskap om Git Flow, som handlar om hur man bör arbeta med Git på ett professionellt sätt.</p>',
             id: 10,
         },
     ],
@@ -86,56 +76,49 @@ export default {
             type: 'wordpress',
             title: 'Wordpress',
             number: '7',
-            html:
-                '<p>Wordpress har varit det CMS vi arbetat med i utbildningen och jag gillar det!</p><p>Jag har byggt ett flertal mindre Wordpress-sajter, både med editorn, men har även gjort egna teman från scratch i PHP för att lära mig hur systemet fungerar under huven.</p><p>Jag har gjort mycket av sajten <a href="https://ankra.jakobg.se">Ankra</a> som var ett större grupparbete vi gjorde vintern 2020. Uppgiften bestod i att göra ett Wordpresstema för en fiktiv bokningssajt för gästhamnar i Sverige.</p><p>Överlag känner jag att jag har en god förståelse för hur Wordpress fungerar, och jag har precis satt igång med ett större Wordpressprojekt med Bedrock och Sage på Fröjd, så jag kommer ha goda kunskaper vid examen.</p><p>Här är <a href="https://github.com/iths-jakob-gauffin/titanic-theme">git-repot till Ankra</a>.</p>',
+            html: '<p>Wordpress har varit det CMS vi arbetat med i utbildningen och jag gillar det!</p><p>Jag har byggt ett flertal mindre Wordpress-sajter, både med editorn, men har även gjort egna teman från scratch i PHP för att lära mig hur systemet fungerar under huven.</p><p>Jag har gjort mycket av sajten <a href="https://ankra.jakobg.se">Ankra</a> som var ett större grupparbete vi gjorde vintern 2020. Uppgiften bestod i att göra ett Wordpresstema för en fiktiv bokningssajt för gästhamnar i Sverige.</p><p>Överlag känner jag att jag har en god förståelse för hur Wordpress fungerar, och jag har precis satt igång med ett större Wordpressprojekt med Bedrock och Sage på Fröjd, så jag kommer ha goda kunskaper vid examen.</p><p>Här är <a href="https://github.com/iths-jakob-gauffin/titanic-theme">git-repot till Ankra</a>.</p>',
             id: 11,
         },
         {
             type: 'php',
             title: 'Php',
             number: '6',
-            html:
-                '<p>Jag har mestadels kommit i kontakt med PHP när jag arbetat med WordPress.</p><p>Jag trivs med språket och har för avsikt att bli lika bra på det som jag är på Javascript.</p><p>Nu på praktiken har jag satt igång med ett PHP-projekt med Wordpress och Sage, där jag skriver PHP tillsammans med Blade.</p>',
+            html: '<p>Jag har mestadels kommit i kontakt med PHP när jag arbetat med WordPress.</p><p>Jag trivs med språket och har för avsikt att bli lika bra på det som jag är på Javascript.</p><p>Nu på praktiken har jag satt igång med ett PHP-projekt med Wordpress och Sage, där jag skriver PHP tillsammans med Blade.</p>',
             id: 12,
         },
         {
             type: 'nodejs',
             title: 'Node.js',
             number: '5',
-            html:
-                '<p>Jag har grundläggande kunskaper i Node.js.</p><p>Jag byggde ett REST-api med CRUD-funktionalitet och API-nyckel-verifiering i Express, våren 2020, till mitt skolprojekt <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>.</p>',
+            html: '<p>Jag har grundläggande kunskaper i Node.js.</p><p>Jag byggde ett REST-api med CRUD-funktionalitet och API-nyckel-verifiering i Express, våren 2020, till mitt skolprojekt <a href="https://hamsterwars-fs-eff-frontend.herokuapp.com">Hamster Wars</a>.</p>',
             id: 13,
         },
         {
             type: 'mysql',
             title: 'SQL',
             number: '4',
-            html:
-                '<p>Jag har grundläggande kunskaper i SQL.</p><p>Jag har gjort några skolprojekt i SQL där jag byggt mindre relationsdatabaser.</p>',
+            html: '<p>Jag har grundläggande kunskaper i SQL.</p><p>Jag har gjort några skolprojekt i SQL där jag byggt mindre relationsdatabaser.</p>',
             id: 14,
         },
         {
             type: 'wagtail',
             title: 'Wagtail',
             number: '5',
-            html:
-                '<p>Jag har kommit i kontakt med Wagtail eftersom att det är Fröjds val av CMS.</p><p>Det känns både kraftfullt, snabbt och användarvänligt.</p><p>För att lära mig Wagtail byggde jag i vintras en bloggsajt med authentication för min familjs lantställe.</p><p>Det är en sajt som jag kommer berätta mer om så småningom.</p>',
+            html: '<p>Jag har kommit i kontakt med Wagtail eftersom att det är Fröjds val av CMS.</p><p>Det känns både kraftfullt, snabbt och användarvänligt.</p><p>För att lära mig Wagtail byggde jag i vintras en bloggsajt med authentication för min familjs lantställe.</p><p>Det är en sajt som jag kommer berätta mer om så småningom.</p>',
             id: 15,
         },
         {
             type: 'python',
             title: 'Python',
             number: '3',
-            html:
-                '<p>Jag har kommit i kontakt med Python tack vare Django och CMS:et Wagtail som Fröjd arbetar med.</p><p>Jag är fortfarande en nybörjare på Python, men hoppas att jag har möjlighet i framtiden att utbilda mig i språket och dess möjligheter.</p>',
+            html: '<p>Jag har kommit i kontakt med Python tack vare Django och CMS:et Wagtail som Fröjd arbetar med.</p><p>Jag är fortfarande en nybörjare på Python, men hoppas att jag har möjlighet i framtiden att utbilda mig i språket och dess möjligheter.</p>',
             id: 16,
         },
         {
             type: 'django',
             title: 'Django',
             number: '4',
-            html:
-                '<p>Jag är en nybörjare på Django och har byggt ett fåtal enklare projekt i ramverket.</p>',
+            html: '<p>Jag är en nybörjare på Django och har byggt ett fåtal enklare projekt i ramverket.</p>',
             id: 17,
         },
     ],
