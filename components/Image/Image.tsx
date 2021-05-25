54fda71025da872332f9addcc1e5a43d8fdfb1f4
iths-jakob-gauffin <54362410+iths-jakob-gauffin@users.noreply.github.com>
2021-05-25 08:44:56 +0200

    Implement react-code-blocks in ConstructionPage

--- components/Image/Image.tsx
+++ components/Image/Image.tsx
@@ -13,7 +13,6 @@ const Image: React.FC<IImageProps> = ({
     round = false,
     modifier = [],
 }) => {
-    console.log("🚀 ~ file: Image.tsx ~ line 16 ~ modifier", modifier)
     const classes = !modifier.length ? styles["Image"] : classNames(modifier.map(x => (
         [x]
     )), styles["Image"]);
--- containers/ConstructionPage/ConstructionPage.tsx
+++ containers/ConstructionPage/ConstructionPage.tsx
@@ -1,10 +1,64 @@
 import styles from "./ConstructionPage.module.scss";
+import {CodeBlock, tomorrowNightBlue} from "react-code-blocks";
 
 interface IConstructionPageProps {
 
 }
 
 const ConstructionPage: React.FC<IConstructionPageProps> = () => {
+    const code = "<div><h1>hejsan</h1></div>";
+
+    const imageCode = `import styles from './Image.module.scss';
+    import classNames from "classnames";
+
+    import { IImageProps, IMediaQueries } from '../../types/types';
+
+    const Image: React.FC<IImageProps> = ({
+        src,
+        width,
+        height,
+        alt,
+        mediaQueries,
+        loading = 'lazy',
+        round = false,
+        modifier = [],
+    }) => {
+        const classes = !modifier.length ? styles["Image"] : classNames(modifier.map(x => (
+            [x]
+        )), styles["Image"]);
+
+        let imageStyles = {};
+
+        if (round) {
+            imageStyles = { borderRadius: "50%" }
+        }
+
+        return (
+            <picture className={classes}
+            >
+                {!!mediaQueries?.length &&
+                    mediaQueries.map(({ src, minWidth }, index) => (
+                        <source
+                            key={index}
+                            media={\`(min-width: \${minWidth}px)\`}
+                            srcSet={src}
+                        />
+                    ))}
+                <img
+                    src={src}
+                    alt={alt}
+                    width={width}
+                    height={height}
+                    loading={loading}
+                    style={imageStyles}
+                    className={styles['Image__Image']}
+                />
+            </picture >
+        );
+    };
+
+    export default Image;
+    `;
   return (
     <div className={styles["ConstructionPage"]}>
       <div className={styles['ConstructionPage__Grid']}>
@@ -16,11 +70,15 @@ const ConstructionPage: React.FC<IConstructionPageProps> = () => {
           <div
             className={styles['ConstructionPage__Preamble']}
           >dssfdsfdsfsd preamlbe</div>
-              <pre>
-                  <span><</span>
-                  <span>p</span>
-                  <span>></span>
-              </pre>
+          <div className={styles["ConstructionPage__CodeWrapper"]}>
+              <CodeBlock
+                  text={imageCode}
+                  language={"jsx"}
+                  showLineNumbers={true}
+                  theme={tomorrowNightBlue}
+                  wrapLines
+              />
+          </div>
 
           <div className={styles['ConstructionPage__ParagraphPres']}>
             {/* <div className={styles['StartContainer__PresImageWrapper']}>
