619859d57124a28bbddab03316aee3c317bc7c77
jakobg123 <jakob.gauffin45@gmail.com>
2021-05-26 07:20:58 +0200

    Implement CodeBlock starter

---
+++ components/CodeBlock/CodeBlock.tsx
@@ -0,0 +1,15 @@
+import styles from "./CodeBlock.module.scss";
+
+interface ICodeBlockProps {
+
+}
+
+const CodeBlock: React.FC<ICodeBlockProps> = () => {
+  return (
+    <div className={styles["CodeBlock"]}>
+
+    </div>
+  );
+};
+
+export default CodeBlock;
