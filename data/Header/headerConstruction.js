0b985b7f4f96bb5a90ff79fa0c0fc4e688d75895
jakobg123 <jakob.gauffin45@gmail.com>
2021-06-08 22:22:58 +0200

    Fix Hero construction mediaQueries and jpgs

--- data/Header/headerConstruction.js
+++ data/Header/headerConstruction.js
@@ -1,21 +1,42 @@
 export default {
     heroImage: {
-        src: '/images/temp/construction_orginal.jpg',
-        alt: 'Målande.',
-        width: 1920,
-        height: 1382,
+        src: '/images/temp/construction_hero_445x300-min.jpg',
+        alt: 'Målarpenslar, målarduk och palett starka turkosa färger.',
+        width: 1442,
+        height: 1038,
         loading: 'eager',
         modifierClass: 'Construction',
         // modifierClass: 'Hero__FocalPoint--Construction',
-        // mediaQueries: [
-        //     {
-        //         minWidth: 768,
-        //         src: '/images/temp/hero_orginal-min.jpg',
-        //     },
-        //     {
-        //         minWidth: 460,
-        //         src: '/images/temp/hero_s-min.jpg',
-        //     },
-        // ],
+        mediaQueries: [
+            {
+                minWidth: 768,
+                src: '/images/temp/construction_hero_1442x700-min.jpg',
+            },
+            {
+                minWidth: 460,
+                src: '/images/temp/construction_hero_754x366-min.jpg',
+            },
+        ],
     },
 };
+
+// export default {
+//     heroImage: {
+//         src: '/images/temp/hero_xss-min.jpg',
+//         alt: 'Skog och sjö en sommarmorgon.',
+//         width: 1440,
+//         height: 1092,
+//         loading: 'eager',
+//         modifierClass: 'Hero__FocalPoint',
+//         mediaQueries: [
+//             {
+//                 minWidth: 768,
+//                 src: '/images/temp/hero_orginal-min.jpg',
+//             },
+//             {
+//                 minWidth: 460,
+//                 src: '/images/temp/hero_s-min.jpg',
+//             },
+//         ],
+//     },
+// };
