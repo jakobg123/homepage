86e7c29142a700b197efa37be0f32836b290061e
jakobg123 <jakob.gauffin45@gmail.com>
2021-08-15 18:34:08 +0200

    remove commented code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,82 @@
 import { PureComponent } from 'react';
-import dynamic from "next/dynamic";
-// import Head from 'next/head';
-//Data for getStaticProps
+import dynamic from 'next/dynamic';
+
 import menuData from '../data/menu.js';
 import knowledgeData from '../data/knowledge.js';
 import startData from '../data/startData.js';
-import logoData from "../data/logo";
-import footerData from "../data/footer";
-import headerData from "../data/header";
-import mainNavData from "../data/mainNav";
-import metaData from "../data/Meta/metaStartPage";
+import logoData from '../data/logo';
+import footerData from '../data/footer';
+import headerData from '../data/header';
+import mainNavData from '../data/mainNav';
+import metaData from '../data/Meta/metaStartPage';
 
-import BaseContainer from "../containers/BaseContainer";
-import Meta from "../components/Meta";
+import BaseContainer from '../containers/BaseContainer';
+import Meta from '../components/Meta';
 import { IMobileNavProps } from '../components/MobileNavButton/MobileNav.jsx';
-import { IKnowledgeData, ILogoData, IStartPageData } from '../types/typesData.jsx';
+import {
+  IKnowledgeData,
+  ILogoData,
+  IStartPageData,
+} from '../types/typesData.jsx';
 import { IFooter } from '../components/Footer/Footer.jsx';
 import { IHeaderProps } from '../components/Header/Header.jsx';
 import { IMainNavProps } from '../components/MainNav/MainNav.jsx';
-import PageContext from "../utils/Page.context";
+import PageContext from '../utils/Page.context';
 // import StartPage from '../containers/StartPage/StartPage.tsx';
-import StartPage from "../containers/StartPage";
+import StartPage from '../containers/StartPage';
 // const StartPage = dynamic(() => import("../containers/StartPage"));
 
 export interface IBaseProps {
-    baseData: {
-        menuData: IMobileNavProps;
-        knowledgeData?: IKnowledgeData;
-        startData?: IStartPageData;
-        logoData: ILogoData;
-        footerData: IFooter;
-        headerData: IHeaderProps;
-        mainNavData: IMainNavProps;
-        metaData: any;
-        constructionPageData: any;
-    };
+  baseData: {
+    menuData: IMobileNavProps;
+    knowledgeData?: IKnowledgeData;
+    startData?: IStartPageData;
+    logoData: ILogoData;
+    footerData: IFooter;
+    headerData: IHeaderProps;
+    mainNavData: IMainNavProps;
+    metaData: any;
+    constructionPageData: any;
+  };
 }
 
-class Base extends PureComponent<IBaseProps>  {
-    state = {};
+class Base extends PureComponent<IBaseProps> {
+  state = {};
 
-    render() {
-        let { baseData } = this.props;
+  render() {
+    let { baseData } = this.props;
 
-        return (
-            <>
-                <Meta {...baseData.metaData} />
-                <PageContext.Provider value="Start">
-                    <BaseContainer {...baseData} page={"Start"}>
-                        <StartPage
-                            knowledge={baseData.knowledgeData}
-                            data={baseData.startData} />
-                    </BaseContainer>
-                </PageContext.Provider>
-            </>
-        );
-    }
+    return (
+      <>
+        <Meta {...baseData.metaData} />
+        <PageContext.Provider value="Start">
+          <BaseContainer {...baseData} page={'Start'}>
+            <StartPage
+              knowledge={baseData.knowledgeData}
+              data={baseData.startData}
+            />
+          </BaseContainer>
+        </PageContext.Provider>
+      </>
+    );
+  }
 }
 
 export async function getStaticProps() {
-    return {
-        props: {
-            baseData: {
-                menuData,
-                knowledgeData,
-                startData,
-                logoData,
-                footerData,
-                headerData,
-                mainNavData,
-                metaData
-            }
-        },
-    };
+  return {
+    props: {
+      baseData: {
+        menuData,
+        knowledgeData,
+        startData,
+        logoData,
+        footerData,
+        headerData,
+        mainNavData,
+        metaData,
+      },
+    },
+  };
 }
 
 export default Base;
